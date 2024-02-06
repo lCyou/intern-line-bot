c068f9f0d94da99028f45312054289ab0d4991d9
kaoru.yamakawa.i <kaoru.yamakawa.i@kaoruyamakawainoMacBook-Pro.local>
2024-02-06 11:38:24 +0900

    add: 外部APIのパラメータの説明をコメント

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -2,7 +2,7 @@ require 'line/bot'
 
 class WebhookController < ApplicationController
   protect_from_forgery except: [:callback] # CSRF対策無効化
-  
+
   def callback
     body = request.body.read
 
@@ -44,6 +44,7 @@ class WebhookController < ApplicationController
 
 
   private
+  hotpepper_api = "http://webservice.recruit.co.jp/hotpepper/gourmet/v1/"
 
   def client
     @client ||= Line::Bot::Client.new { |config|
@@ -52,10 +53,14 @@ class WebhookController < ApplicationController
     }
   end
 
+  
   def search_shop(lat, lng)
-    hotpepper_api = "http://webservice.recruit.co.jp/hotpepper/gourmet/v1/"
     uri = URI.parse(hotpepper_api)
     http = Net::HTTP.new(uri.host, uri.port) 
+    # hotpepper apiのパラメータ
+    # range 3: 1000m以内
+    # genre G001:居酒屋, G002:ダイニングバー・バル
+    # budget B002:2001~3000円, B003:3001~4000円
     uri.query = URI.encode_www_form({
         key: ENV["API_KEY"],
         lat: lat,
