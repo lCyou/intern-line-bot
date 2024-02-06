b86fc0d1c54f56834c7f7ced268c083d4152acfb
kaoru.yamakawa.i <kaoru.yamakawa.i@kaoruyamakawainoMacBook-Pro.local>
2024-02-06 11:10:54 +0900

    fix: メソッドのアクセス制限を付与

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -2,6 +2,48 @@ require 'line/bot'
 
 class WebhookController < ApplicationController
   protect_from_forgery except: [:callback] # CSRF対策無効化
+  
+  def callback
+    body = request.body.read
+
+    signature = request.env['HTTP_X_LINE_SIGNATURE']
+    unless client.validate_signature(body, signature)
+      head 470
+    end
+
+    events = client.parse_events_from(body)
+    events.each { |event|
+      case event
+      when Line::Bot::Event::Message
+        case event.type
+        when Line::Bot::Event::MessageType::Text
+          message = {
+            type: 'text',
+            text: event.message['text']
+          }
+          p message
+          client.reply_message(event['replyToken'], message)
+        when Line::Bot::Event::MessageType::Location
+          res_text = search_shop(event.message['latitude'], event.message['longitude'])
+          message = {
+            "type": "template",
+            "altText": "おすすめのお店情報が届きました！",
+            "template": {
+              "type": "carousel",
+              "columns": res_text,
+              "imageAspectRatio": "rectangle",
+              "imageSize": "cover"
+            }
+          }
+          client.reply_message(event['replyToken'], message)
+        end
+      end
+    }
+    head :ok
+  end
+
+
+  private
 
   def client
     @client ||= Line::Bot::Client.new { |config|
@@ -50,42 +92,4 @@ class WebhookController < ApplicationController
     end
   end
 
-  def callback
-    body = request.body.read
-
-    signature = request.env['HTTP_X_LINE_SIGNATURE']
-    unless client.validate_signature(body, signature)
-      head 470
-    end
-
-    events = client.parse_events_from(body)
-    events.each { |event|
-      case event
-      when Line::Bot::Event::Message
-        case event.type
-        when Line::Bot::Event::MessageType::Text
-          message = {
-            type: 'text',
-            text: event.message['text']
-          }
-          p message
-          client.reply_message(event['replyToken'], message)
-        when Line::Bot::Event::MessageType::Location
-          res_text = search_shop(event.message['latitude'], event.message['longitude'])
-          message = {
-            "type": "template",
-            "altText": "おすすめのお店情報が届きました！",
-            "template": {
-              "type": "carousel",
-              "columns": res_text,
-              "imageAspectRatio": "rectangle",
-              "imageSize": "cover"
-            }
-          }
-          client.reply_message(event['replyToken'], message)
-        end
-      end
-    }
-    head :ok
-  end
 end
